220f6499a8de56dbcb8cee4670582d7830692514
Liu Lantao <liulantao@gmail.com>
2016-11-04 00:51:21 +0800

    add hacker-news-api

--- readom-api-server.rb
+++ readom-api-server.rb
@@ -5,6 +5,8 @@ require 'rubygems'
 require 'bundler'
 Bundler.require
 
+require 'json'
+
 class ReadomAPIServer < Sinatra::Base
 
   enable :sessions
@@ -18,7 +20,44 @@ class ReadomAPIServer < Sinatra::Base
     alias_method :h, :escape_html
   end
 
-  get '/' do
-    'Readom API Server'
+  get '/:ext?' do |ext|
+    case ext
+      when 'json'
+        content_type 'application/json'
+        '{"status":"OK", "info":"Readom API Server"}'
+      else
+        content_type 'text/plain'
+        'Readom API Server'
+    end
+  end
+
+  get "/news/v0/:board.:ext" do |board, ext|
+    base_uri = 'https://hacker-news.firebaseio.com/v0/'
+    firebase = Firebase::Client.new(base_uri)
+    list = firebase.get(board).body
+
+    case ext
+      when 'json'
+        content_type 'application/json'
+        list.to_json
+      else
+        content_type 'text/plain'
+        list.join ','
+    end
+  end
+
+  get '/news/v0/item/:item_id.:ext' do |item_id, ext|
+    base_uri = 'https://hacker-news.firebaseio.com/v0/item/'
+    firebase = Firebase::Client.new(base_uri)
+    item = firebase.get('%s' % item_id).body
+
+    case ext
+      when 'json'
+        content_type 'application/json'
+        item.to_json
+      else
+        content_type 'text/plain'
+        item.to_json
+    end
   end
 end
